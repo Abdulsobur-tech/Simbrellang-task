24378676e861f065ebd8cddac7fe7812c6060902
Abdulsobur <91859818+Abdulsobur-tech@users.noreply.github.com>
2025-01-06 10:47:36 +0100

    add global exception file

---
+++ src/main/java/sembrella/ng/simrella/ng/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package sembrella.ng.simrella.ng.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(DuplicateException.class)
+    public ResponseEntity<String> handleDuplicateRecord(DuplicateException ex){
+        return  new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public  ResponseEntity<String> handleGenericException(Exception ex){
+        return  new ResponseEntity<>("An unexpected error occurred " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("message", ex.getMessage());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(LoanEligibilityException.class)
+    public ResponseEntity<String> handleLoanEligibilityException(LoanEligibilityException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
