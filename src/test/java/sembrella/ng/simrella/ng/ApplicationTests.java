47b439b1da494e2a04445e66ccab0e75dccddbd4
Abdulsobur <91859818+Abdulsobur-tech@users.noreply.github.com>
2025-01-06 10:49:01 +0100

    initial commit

---
+++ src/test/java/sembrella/ng/simrella/ng/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sembrella.ng.simrella.ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
