8e416d47319b241ad69fe5416cf7b7e8ba77d7ac
Abdulsobur <91859818+Abdulsobur-tech@users.noreply.github.com>
2025-01-06 10:42:44 +0100

    add a user data transfer object that enable to make on the data user needs to see.

---
+++ src/main/java/sembrella/ng/simrella/ng/dto/UserDto.java
@@ -0,0 +1,92 @@
+package sembrella.ng.simrella.ng.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class UserDto {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public UserDto(UUID id, String firstName, String lastName, String email, String phoneNumber, LocalDateTime created_at, LocalDateTime updated_at) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public UserDto() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+}
